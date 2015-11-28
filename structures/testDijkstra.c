bcdd5c9c14e417fe68ac45ef894617cb4f1c6cb1
Marlon Mendes <marlonacademy@hotmail.com>
2015-11-28 17:24:01 -0200

    Refactoring tests for Dijkstra

--- structures/testDijkstra.c
+++ structures/testDijkstra.c
@@ -3,35 +3,29 @@
 int main() {
 
     // BUILDING THE GRAPH
-
     Graph *graph = malloc(sizeof(Graph));
     int numberOfVertex = 5;
 
     initGraph(graph, numberOfVertex);
 
-    addEdge(graph, 0, 1, 1);
+    addEdge(graph, 0, 1, 1); 
     addEdge(graph, 0, 2, 8);
     addEdge(graph, 0, 3, 3);
-
-    addEdge(graph, 1, 2, 5);
-
+    addEdge(graph, 1, 2, 5);    // distance between vertex 1 and 2 is 5
     addEdge(graph, 3, 4, 1);
-
     addEdge(graph, 4, 2, 1);
-    
-    //printGraph(graph);
 
     // BUILDING DIJKSTRA;
     
     Dijkstra *dijkstra = malloc(sizeof(Dijkstra));
-    int source = 0,
-        target = 2;
+    int source = 0,            // leaving from "source"
+        target = 2;            // goint to "target"
     initDijkstra(dijkstra, graph, source);
+
     int distance = shortestDistance(dijkstra, target);
     printf("shortest distance from %d to %d is = %d\n", source,
                                                         target,
                                                         distance);
-
     // DESTROYING (FREEING) STRUCTURES
     destroyDijkstra(dijkstra);
     destroyGraph(graph);
