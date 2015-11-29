67dff04dc36469ff8827eefb7b6f822293460027
Julio Xavier <juliobepid@hotmail.com>
2015-11-29 00:21:19 -0200

    Correcting test

--- structures/testDijkstra.c
+++ structures/testDijkstra.c
@@ -9,17 +9,28 @@ int main() {
     initGraph(graph, numberOfVertex);
 
     addEdge(graph, 0, 1, 1); 
+    addEdge(graph, 1, 0, 1);
+
     addEdge(graph, 0, 2, 8);
+    addEdge(graph, 2, 0, 8);
+
     addEdge(graph, 0, 3, 3);
+    addEdge(graph, 3, 0, 3);
+
     addEdge(graph, 1, 2, 5);    // distance between vertex 1 and 2 is 5
+    addEdge(graph, 2, 1, 5);
+
     addEdge(graph, 3, 4, 1);
+    addEdge(graph, 4, 3, 1);
+
     addEdge(graph, 4, 2, 1);
+    addEdge(graph, 2, 4, 1);
 
     // BUILDING DIJKSTRA;
     
     Dijkstra *dijkstra = malloc(sizeof(Dijkstra));
-    int source = 0,            // leaving from "source"
-        target = 2;            // goint to "target"
+    int source = 2,            // leaving from "source"
+        target = 0;            // goint to "target"
     initDijkstra(dijkstra, graph, source);
 
     int distance = shortestDistance(dijkstra, target);
